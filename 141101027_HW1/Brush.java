e0c5fb67e426d69157203e1606822cdd13282453
berksigin <asigin@etu.edu.tr>
2020-02-11 09:40:21 +0300

    Interesting

--- 141101027_HW1/Brush.java
+++ 141101027_HW1/Brush.java
@@ -3,6 +3,10 @@ public class Brush {
 	public String name;
 	public Brush(String n) {name = n; height = 1; width = 1;}
 	public Brush(String n,int h, int w) {name = n; height = h; width = w;}
+	public Brush(Token i1, Token i2) {
+		height = Integer.parseInt(String.valueOf(i1));
+		width = Integer.parseInt(String.valueOf(i2));
+	}
 	public String toString() {return height + " " + width;}
 	public void setHeight(int h) {height = h;}
 	public void setWidth(int w) {width = w;}
